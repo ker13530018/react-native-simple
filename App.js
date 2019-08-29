e07f8e70b03fe80f54cc03b7990b007aaeef6e70
Jirayut Khantavee <jirayut.kh@kbtg.tech>
2019-08-29 13:51:46 +0700

    Add simple reducer

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './src/home'
 import DocumentScreen from './src/document'
+import SquareScreen from './src/square'
 
 const AppNavigator = createStackNavigator(
   {
@@ -17,9 +18,12 @@ const AppNavigator = createStackNavigator(
         title: 'Document',
       },
     },
+    Square: {
+      screen: SquareScreen,
+    },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Square',
   },
 )
 
---
+++ src/square/index.js
@@ -0,0 +1,124 @@
+import React, { useReducer } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const reducer = (state, { type, amount }) => {
+  switch (type) {
+    case 'red':
+      if (state.red + amount > 255 || state.red + amount < 0) return state
+      return { ...state, red: state.red + amount }
+    case 'blue':
+      if (state.blue + amount > 255 || state.blue + amount < 0) return state
+      return { ...state, blue: state.blue + amount }
+    case 'green':
+      if (state.green + amount > 255 || state.green + amount < 0) return state
+      return { ...state, green: state.green + amount }
+    default:
+      return state
+  }
+}
+
+const SquareScreen = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    red: 255,
+    blue: 255,
+    green: 255,
+  })
+
+  const { red, blue, green } = state
+  //
+  return (
+    <View
+      style={{
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Text style={{ textAlign: 'center', marginTop: 5 }}>Red ({red})</Text>
+      <View
+        style={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+        }}
+      >
+        <Button
+          title="+"
+          onPress={() => {
+            dispatch({ type: 'red', amount: 1 })
+          }}
+          style={{ flex: 1 }}
+        />
+        <Button
+          title="-"
+          onPress={() => {
+            dispatch({ type: 'red', amount: -1 })
+          }}
+          style={{ flex: 1 }}
+        />
+      </View>
+      <Text style={{ textAlign: 'center', marginTop: 5 }}>Blue ({blue})</Text>
+      <View
+        style={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+        }}
+      >
+        <Button
+          title="+"
+          onPress={() => {
+            dispatch({ type: 'blue', amount: 1 })
+          }}
+          style={{ flex: 1 }}
+        />
+        <Button
+          title="-"
+          onPress={() => {
+            dispatch({ type: 'blue', amount: -1 })
+          }}
+          style={{ flex: 1 }}
+        />
+      </View>
+      <Text style={{ textAlign: 'center', marginTop: 5 }}>Green ({green})</Text>
+      <View
+        style={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+        }}
+      >
+        <Button
+          title="+"
+          onPress={() => {
+            dispatch({ type: 'green', amount: 1 })
+          }}
+          style={{ flex: 1 }}
+        />
+        <Button
+          title="-"
+          onPress={() => {
+            dispatch({ type: 'green', amount: -1 })
+          }}
+          style={{ flex: 1 }}
+        />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: `rgb(${red},${blue},${green})`,
+          width: 150,
+          height: 150,
+        }}
+      ></View>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({})
+
+export default SquareScreen
